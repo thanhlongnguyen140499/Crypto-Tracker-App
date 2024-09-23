0cee0d7472c6f95afe95b23d963525f3076cf052
Edward <edward@enclave.vn>
2024-09-23 20:04:19 +0700

    Initial Commit

---
+++ EverestCrypto/EverestCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverestCryptoApp.swift
+//  EverestCrypto
+//
+//  Created by Edward Nguyen on 23/9/24.
+//
+
+import SwiftUI
+
+@main
+struct EverestCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
